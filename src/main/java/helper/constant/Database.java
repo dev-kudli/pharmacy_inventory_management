82284364a17c32e26fb0c000ac382fc47c200add
Sudarshan Kudli <kudli.s@northeastern.edu>
2022-12-07 11:04:25 -0500

    Database constants

---
+++ src/main/java/helper/constant/Database.java
@@ -0,0 +1,20 @@
+package helper.constant;
+
+public abstract class Database {
+    private static final String JDBC_URL = "jdbc:mysql://localhost/";
+    private static final String DATABASE = "pharmacy_inventory_management";
+    private static final String USER = "root";
+    private static final String PASSWORD = "localhost";
+    
+    public static String getConnectionUrl() {
+        return JDBC_URL + DATABASE;
+    }
+    
+    public static String getUser() {
+        return USER;
+    }
+    
+    public static String getPassword() {
+        return PASSWORD;
+    }
+}
