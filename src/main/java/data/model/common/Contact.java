7d940dcaf82e190e51472282998b38192f5bc145
Sudarshan Kudli <kudli.s@northeastern.edu>
2022-12-07 13:44:56 -0500

    Common classes - Contact and Date

---
+++ src/main/java/data/model/common/Contact.java
@@ -0,0 +1,15 @@
+package data.model.common;
+
+public class Contact {
+    public String city;
+    public String state;
+    public String zip;
+    public String address;
+    
+    public Contact(String city, String state, String zip, String address) {
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.address = address;
+    }
+}
