d505a0844e1568f2f2d4b90fd9764a45af504b5f
Sudarshan Kudli <kudli.s@northeastern.edu>
2022-12-07 11:43:02 -0500

    Defined role constants

---
+++ src/main/java/helper/constant/UserRole.java
@@ -0,0 +1,10 @@
+package helper.constant;
+
+public abstract class UserRole {
+    public static final String SYSTEM_ADMIN = "sys_adm";
+    public static final String PHARMACY_ADMIN = "pharma_adm";
+    public static final String STORE_ADMIN = "store_adm";
+    public static final String CUSTOMER = "customer";
+    public static final String MANUFACTURE_ADMIN = "man_adm";
+    public static final String DISTRIBUTOR_ADMIN = "dist_adm";
+}
