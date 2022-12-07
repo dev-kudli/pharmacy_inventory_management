d027e7f20806178a518937168d85f9a48f43f1c8
Shreya Jaiswal <shreyajaiswal1604@gmail.com>
2022-12-07 15:27:48 -0500

    pharmacy-model-classes

---
+++ src/main/java/data/model/common/Company.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data.model.common;
+
+/**
+ *
+ * @author shreyajaiswal
+ */
+public class Company {
+    
+    private String companyName;
+    private String companyType;
+    private String registeredDate;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyType() {
+        return companyType;
+    }
+
+    public void setCompanyType(String companyType) {
+        this.companyType = companyType;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+    
+    public Company(String companyType){
+        this.companyType = companyType;
+    }
+    
+}
--- src/main/java/data/model/person/Person.java
+++ src/main/java/data/model/person/Person.java
@@ -4,10 +4,89 @@
  */
 package data.model.person;
 
+import data.model.common.Contact;
+
 /**
  *
  * @author shreyajaiswal
  */
 public class Person {
-    
+     String id;
+     private String personName;
+     private String personType;
+     private String personDob;
+     private String personGender;
+     private Contact personContact;
+     
+     public Person(String id) {
+
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonType() {
+        return personType;
+    }
+
+    public void setPersonType(String personType) {
+        this.personType = personType;
+    }
+
+    public String getPersonDob() {
+        return personDob;
+    }
+
+    public void setPersonDob(String personDob) {
+        this.personDob = personDob;
+    }
+
+    public String getPersonGender() {
+        return personGender;
+    }
+
+    public void setPersonGender(String personGender) {
+        this.personGender = personGender;
+    }
+
+    public Contact getPersonContact() {
+        return personContact;
+    }
+
+    public void setPersonContact(Contact personContact) {
+        this.personContact = personContact;
+    }
+     
+
+ 
+
+    public String getPersonId() {
+        return id;
+    }
+
+    public boolean isMatch(String id) {
+        if (getPersonId().equals(id)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getPersonId();
+    }
 }
