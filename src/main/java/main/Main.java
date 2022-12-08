c5586cef09573c805c8348ce9396fb1530a46de4
Sudarshan Kudli <kudli.s@northeastern.edu>
2022-12-07 19:23:12 -0500

    Pharmacy db modules

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,10 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Pharma Inventory Management");
+        try {
+            System.out.println("Welcome to Pharma Inventory Management");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
