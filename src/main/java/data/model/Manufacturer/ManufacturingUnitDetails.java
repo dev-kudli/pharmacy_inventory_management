f79e38b5572d7326c80db4d2d6118fac823dfe23
Alankrutha18 <purumandla.a@northeastern.edu>
2022-12-07 19:34:54 -0500

    Adding changes to ManufacturingUnitDetails

--- src/main/java/data/model/Manufacturer/ManufacturingUnitDetails.java
+++ src/main/java/data/model/Manufacturer/ManufacturingUnitDetails.java
@@ -4,10 +4,35 @@
  */
 package data.model.Manufacturer;
 
+import data.model.common.Location;
+
 /**
  *
  * @author alank
  */
 public class ManufacturingUnitDetails {
+    private int Manufacturing_company_id;
+    private Location Manufacturing_Unit_location;
+
+    public int getManufacturing_company_id() {
+        return Manufacturing_company_id;
+    }
+
+    public void setManufacturing_company_id(int Manufacturing_company_id) {
+        this.Manufacturing_company_id = Manufacturing_company_id;
+    }
+
+    public Location getManufacturing_unit_location() {
+        return Manufacturing_Unit_location;
+    }
+
+    public void setManufacturing_Unit_location(Location Manufacturing_Unit_location) {
+        this.Manufacturing_Unit_location = Manufacturing_Unit_location;
+    }
+    
+    
     
 }
+
+    
+
