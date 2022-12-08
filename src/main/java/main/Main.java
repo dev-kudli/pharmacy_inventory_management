889d52e1ed6d1c89904227a90046c10a76c498ec
Shreya Jaiswal <shreyajaiswal1604@gmail.com>
2022-12-08 16:31:33 -0500

    Pharmacy adm panel

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import ui.frame.MainFrame;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Pharma Inventory Management");
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+        frame.showPharmacyAdminPanel();
     }
 }
