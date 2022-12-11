f2f16ca7213d356e8cd9d61f71f19757857bf82b
Sudarshan Kudli <kudli.s@northeastern.edu>
2022-12-10 23:00:52 -0500

    transport panel and backend

---
+++ src/main/java/db/TransportManager.java
@@ -0,0 +1,39 @@
+package db;
+
+import static db.PersonManager.con;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class TransportManager {
+    public static int confirmShipmentStatus(int shipmentId) throws Exception {
+        try {
+            String queryToUpdateStatus = """
+                UPDATE shipment
+                SET shipment_status=completed
+                WHERE shipment_id=%s""";
+            queryToUpdateStatus = String.format(queryToUpdateStatus, shipmentId);
+            Statement stmt = con.createStatement();
+            return stmt.executeUpdate(queryToUpdateStatus);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+    public static ResultSet fetchAllShipments(int transporterId) throws Exception {
+        try {
+            String queryToFetchShipments = """
+                SELECT s.shipment_id, p.order_date, p.pharmacy_id, c1.company_name AS pharmacy_name, c2.company_name AS distributor_name, s.shipment_status
+                FROM shipment s
+                JOIN pharmacy_order p ON p.order_id=s.order_id
+                JOIN company c1 ON p.pharmacy_id=c1.company_id
+                JOIN company c2 ON s.distributor_id=c1.company_id
+                WHERE c1.transporter_id=%s""";
+            queryToFetchShipments = String.format(queryToFetchShipments, transporterId);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(queryToFetchShipments);
+            return rs;
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+}
